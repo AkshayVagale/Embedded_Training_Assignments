507009b8c89846621a096536986d6414e32e2a03
Arjun <36031200+A-r-jun@users.noreply.github.com>
2021-06-15 19:44:40 +0530

    Create Assignment2_1.c

---
+++ Assignment2/Assignment2_1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+
+int main()
+{	
+	int x = 10, y = 3;
+	{
+		int x = 100, z = 10;
+		printf("%d %d", x, z);
+	}
+	printf("%d %d", x, z); // this will through error that z is undeclared
+ }
+
+
