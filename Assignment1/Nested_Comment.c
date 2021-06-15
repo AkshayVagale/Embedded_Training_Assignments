e754e0e09c80870a0ff1151ea7eef1673c22943b
Arjun <36031200+A-r-jun@users.noreply.github.com>
2021-06-15 16:42:19 +0530

    Assignment1.c

--- Assignment1/Nested_Comment.c
+++ Assignment1/Nested_Comment.c
@@ -2,13 +2,28 @@
 
 int main()
 {
+  // the block of code inside the #if 0 _____ #endif wont compile
+#if 0 
   int i;
   char c;
+  f= i+c;
+#endif
   
   /*This is comment
     /*This is nested comment*/
   */
  // This will give error because the multiline comment will end with the first "*/" and next "*/" will through error     
     
-  return 0
+    char ch1, ch2, ch3;
+	ch1 = 65;
+	ch2 = 'b';
+	ch3 = '3';                   //  Output
+	printf("ch1 = %c\n",ch1);    //   'A'
+	printf("ch1 = %c\n", ch2);   //   'b' 
+	printf("ch1 = %c\n", ch3);   //   '3'
+	printf("ch1 = %d\n", ch1);   //   65
+	printf("ch1 = %d\n", ch2);   //   98
+	printf("ch1 = %d\n", ch3);   //   51
+	return 0;
+
 }
