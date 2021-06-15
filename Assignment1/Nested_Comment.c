d50e3e87a22129cb62657b9df1c13aabba67fc5a
Arjun <36031200+A-r-jun@users.noreply.github.com>
2021-06-15 16:35:22 +0530

    Update Nested_Comment.c

--- Assignment1/Nested_Comment.c
+++ Assignment1/Nested_Comment.c
@@ -8,6 +8,7 @@ int main()
   /*This is comment
     /*This is nested comment*/
   */
-
+ // This will give error because the multiline comment will end with the first "*/" and next "*/" will through error     
+    
   return 0
 }
