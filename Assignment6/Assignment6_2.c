7f4b99fa532281031fecb77a135e9f71c573a3a1
Arjun <36031200+A-r-jun@users.noreply.github.com>
2021-06-22 01:15:55 +0530

    Create Assignment6_2.c

---
+++ Assignment6/Assignment6_2.c
@@ -0,0 +1,29 @@
+/* Program to find if the array is sorted in ascending order or not */
+
+#include<stdio.h>
+int main()
+{
+	int array[100] = { 0 }, size,flag=1;
+		printf("Enter the no of element :");
+	scanf_s("%d", &size);
+	printf("Enter the element :\n");
+	for (int i = 0;i < size;i++)
+	{
+		scanf_s("%d", &array[i]);
+	}
+
+	for (int i = 0;i < size - 1;i++)
+	{
+		if (array[i] > array[i + 1])
+		{
+			printf(" Array is not sorted in ascending order");
+			flag = 0;
+			break;
+		}
+	}
+
+	if(flag)
+	printf(" Array is sorted in ascending order");
+
+	return 0;
+}
