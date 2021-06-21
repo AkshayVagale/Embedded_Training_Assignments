8175df6b31e8d9b7048c5ab239a5229fe630526f
Arjun <36031200+A-r-jun@users.noreply.github.com>
2021-06-21 22:47:10 +0530

    Update Assignment4_1.c

--- Assignment4/Assignment4_1.c
+++ Assignment4/Assignment4_1.c
@@ -1,5 +1,16 @@
 /* Command to display current time on linux with milliseconds and nanoseconds */
 
  date +%3N:%N // this will print Hour:Minutes:Seconds:Milliseconds:Nanoseconds
-   
+ 
+ 
+ 
+ /* 
+ Why inline function needs static storage class ?
+ 
+ Function declared with static class will make it locally visible only i.e same source file only.
+ Function declared with inline tells compiler to generate the code for the function's block right where the function is called.
+ So static inline funciton is used to make it invisible , normally used for very small ,simple and frequently called helper function.
+ 
+ */
+ 
    
